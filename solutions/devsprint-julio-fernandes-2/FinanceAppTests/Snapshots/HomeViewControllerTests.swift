806ad240a8ca570222779ac37c89eaaed5f3fb35
Dairan <dairan@dairan.com>
2022-04-19 22:19:11 -0300

    testes na HomeViewController

---
+++ solutions/devsprint-julio-fernandes-2/FinanceAppTests/Snapshots/HomeViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewControllerTests.swift
+//  FinanceAppTests
+//
+//  Created by Dairan on 19/04/22.
+//
+
+import SnapshotTesting
+import XCTest
+
+class HomeViewControllerTests: XCTestCase {
+    func test_HomeViewController() throws {
+        
+    }
+}
